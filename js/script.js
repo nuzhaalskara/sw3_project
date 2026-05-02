b40186e73cb9907139fb7851453d7efd64de9188
nuzhaalskara <nuzhaalskara123@gmail.com>
2026-05-03 00:18:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function register() {
+    alert("Account created successfully!");
+}
+
+function bookAppointment() {
+    alert("Appointment booked!");
+}
+
+function addUser() {
+    alert("User added!");
+}
